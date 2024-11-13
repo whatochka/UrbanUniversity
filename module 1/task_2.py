c638d02d70adc966bdf4eeff0df32d9432074fcd
whtch <whatochka@gmail.com>
2024-11-13 14:24:17 +0300

    complite task 2

---
+++ module 1/task_2.py
@@ -0,0 +1,8 @@
+num_of_homework_comp = 12
+num_of_hours_spent = 1.5
+course_title = "Python"
+time_per_task = num_of_hours_spent / num_of_homework_comp
+
+print(f"Курс: {course_title}, всего задач: {num_of_homework_comp}, "
+      f"затрачено часов: {num_of_hours_spent}, "
+      f"среднее время выполнения: {time_per_task} часа.")
