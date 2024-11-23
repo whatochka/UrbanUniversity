68509d89053601ddcf9f0f7332fd41a70dc3c408
whtch <whatochka@gmail.com>
2024-11-23 16:49:01 +0300

    complite module 2 task 2

---
+++ module 2/task_2.py
@@ -0,0 +1,13 @@
+num_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+num_list_length = len(num_list)
+index = 0
+
+while True:
+    if num_list[index] > 0:
+        print(num_list[index])
+        index += 1
+    else:
+        index += 1
+
+    if index == num_list_length:
+        break
