f6eada904b91d117b15f40cafe765746ceafd935
whtch <whatochka@gmail.com>
2024-12-10 00:53:30 +0300

    complite module 3, task 2

---
+++ module 3/task_2.py
@@ -0,0 +1,24 @@
+def send_email(message, recipient, sender="university.help@gmail.com"):
+    default_sender = "university.help@gmail.com"
+
+    def is_valid_email(email):
+        return "@" in email and (email.endswith(".com") or email.endswith(".ru") or email.endswith(".net"))
+
+    if not is_valid_email(recipient) or not is_valid_email(sender):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+        return False
+
+    if recipient == sender:
+        print("Нельзя отправить письма самому себе!")
+        return False
+
+    if sender == default_sender:
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
