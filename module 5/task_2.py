4e35d0bfdc00b0a06c500d2a8fcd0a6fa6d9eacd
3.14ka <321_makson_123@mail.ru>
2025-01-08 18:47:33 +0300

    complite module 5, task 2

---
+++ module 5/task_2.py
@@ -0,0 +1,30 @@
+class House:
+    def __init__(self, name, floors):
+        self.name = name
+        self.number_of_floors = floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors:
+            print("Такого этажа не существует")
+            return
+
+        for num_of_floor in range(1, new_floor + 1):
+            print(num_of_floor)
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
